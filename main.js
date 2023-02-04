bd1d9d734f3127de86dfe22903b6b7ac332c55fd
callixmiller <calmill413@gmail.com>
2023-02-03 22:05:48 -0500

    main.js heart selectable, becomes full and can deselect a liked item

--- main.js
+++ main.js
@@ -3,9 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const liked = document.getElementsByClassName('like-glyph')
+for (let heart of liked) {
+  heart.addEventListener("click", handleLike);
+}
 
-
-
+  function handleLike(event) {
+    mimicServerCall()
+      .then(() => {
+        event.target.classList.toggle('activated-heart')
+        if (event.target.innerText === EMPTY_HEART) {
+          event.target.innerText = FULL_HEART
+        } else {
+          event.target.innerText = EMPTY_HEART
+        }
+      })
+      .catch(() => {
+        const errMsg = document.getElementById('modal')
+        errMsg.className = ""
+        setTimeout(() => {errMsg.className = 'hidden'}, 3000)
+      })
+  }
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
